ff4545132d8269820b564e3b15b77b9266196a7f
allzerooo <dychoi.dev@gmail.com>
2026-01-05 15:22:27 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/toy/first_come_grant_service/FirstComeGrantServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.first_come_grant_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstComeGrantServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstComeGrantServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/toy/first_come_grant_service/FirstComeGrantServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.first_come_grant_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstComeGrantServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
